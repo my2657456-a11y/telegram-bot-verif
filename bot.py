ad796ee89d43fd13c282f303bb6c2d9bfadcaa5e
my2657456-a11y <my2657456@gmail.com>
2026-04-22 12:11:38 +0700

    Add files via upload
    Membantu pantau anti fake member

---
+++ bot.py
@@ -0,0 +1,64 @@
+import random
+import asyncio
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, ChatMemberHandler
+
+BOT_TOKEN = os.getenv("8696238507:AAG9F1QR5DSf1e20ZbzEqGK22Bn0aK8eK1E")
+
+pending_users = {}
+
+async def new_member(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    result = update.chat_member
+    user = result.new_chat_member.user
+
+    if result.old_chat_member.status in ["left", "kicked"] and result.new_chat_member.status == "member":
+        if user.is_bot:
+            return
+
+        chat_id = update.effective_chat.id
+        user_id = user.id
+
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        answer = a + b
+
+        pending_users[user_id] = {"answer": answer, "chat_id": chat_id}
+
+        await context.bot.send_message(
+            chat_id,
+            f"Halo {user.first_name}! Verifikasi dulu ya!\n\nBerapa {a} + {b}? (60 detik)"
+        )
+
+        asyncio.create_task(timeout_kick(context, chat_id, user_id, user.first_name))
+
+async def timeout_kick(context, chat_id, user_id, name):
+    await asyncio.sleep(60)
+
+    if user_id in pending_users:
+        await context.bot.ban_chat_member(chat_id, user_id)
+        del pending_users[user_id]
+
+        await context.bot.send_message(chat_id, f"{name} tidak verifikasi. Auto kick!")
+
+async def check_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+
+    if user_id in pending_users:
+        correct = pending_users[user_id]["answer"]
+
+        try:
+            if int(update.message.text) == correct:
+                del pending_users[user_id]
+                await update.message.reply_text("Verifikasi berhasil!")
+            else:
+                await update.message.reply_text("Jawaban salah!")
+        except:
+            pass
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(ChatMemberHandler(new_member, ChatMemberHandler.CHAT_MEMBER))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_answer))
+
+app.run_polling()
